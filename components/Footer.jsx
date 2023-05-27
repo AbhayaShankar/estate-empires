58cf45994ab0c066337bb348f3d221fcad4d460a
Abhaya <shankarabhaya9@gmail.com>
2023-05-27 23:13:55 +0530

    search compoennt started

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,7 +1,22 @@
-import React from "react";
+import { Box } from "@chakra-ui/react";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <Box
+      p={5}
+      textAlign={"center"}
+      color={"#94A187"}
+      borderTop={1}
+      borderColor={"#94A187"}
+      fontWeight={"semibold"}
+      letterSpacing={0.4}
+      fontSize={15}
+      fontFamily={"Montserrat , sans-serif"}
+      style={{ borderTop: "1px solid #D0CDD7" }}
+    >
+      2023 © Estate Empires Inc.
+    </Box>
+  );
 };
 
 export default Footer;
